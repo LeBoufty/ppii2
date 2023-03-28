1b4144a6c65acf1f2439ffeae00086fc598af893
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-03-28 10:13:11 +0200

    Suppression des stations à moins d'1km d'une autre

--- BD/filtrestations.py
+++ BD/filtrestations.py
@@ -1,19 +1,25 @@
 from outils import *
 # Ce script python douteux sert à filtrer les stations et les données qu'on veut garder.
 # Il a besoin de la version modifiée de la BD fournie. Elle se trouve sur mon (PYJ) PC (car dans le gitignore)
-data = [i.replace('\n', '').replace('"[', '').replace(']"', '').split(',') for i in open('consolidation-etalab-schema-irve-statique-v-2.2.0-20230323.csv', 'r').readlines()]
-sortie = open('stations.csv', 'a')
+data = [i.replace('\n', '').replace('"[', '').replace(']', '').replace(' ', '').replace('"', '').split(',') for i in open('BD/consolidation-etalab-schema-irve-statique-v-2.2.0-20230323.csv', 'r').readlines()]
+
+# Rafraichissement de la BD
+sortie = open('BD/stations.csv', 'w')
+sortie.write('')
+sortie.close()
+
+sortie = open('BD/stations.csv', 'a')
 
 xy_trouves = []
-
-def a_proximite(x, y):
-    pass
 
 for ligne in data:
     if len(ligne) == 4:
         id, x, y, acces = ligne
-        if (acces == "Accès libre") and ((x,y) not in xy_trouves) and ("Non" not in id):
+        x = float(x)
+        y = float(y)
+        if (acces == "Accèslibre") and ((x,y) not in xy_trouves) and ("Non" not in id) and not a_proximite(x, y, xy_trouves, 1):
             xy_trouves.append((x,y))
-            sortie.write((id + ',' + x + ',' + y + '\n').replace(' ', ''))
+            sortie.write((id + ',' + str(x) + ',' + str(y) + '\n').replace(' ', ''))
 
+print('Fini !')
 sortie.close()
--- BD/outils.py
+++ BD/outils.py
@@ -9,4 +9,17 @@ def distance(x1, y1, x2, y2):
     y2 = radians(y2)
     
     A = sin(y1)*sin(y2) + cos(y1)*cos(y2)*cos(x2-x1)
+    # Gestion des erreurs de flottant
+    if A > 1: A = 1
+    if A < -1: A = -1 
+    
     return 6371 * acos(A)
+
+def a_proximite(x, y, xy_trouves, distmin = 5):
+    i = 0
+    if len(xy_trouves) == 0: return False
+    x2, y2 = xy_trouves[0]
+    while i < len(xy_trouves) and distance(x, y, x2, y2) > distmin:
+        x2, y2 = xy_trouves[i]
+        i += 1
+    return distance(x, y, x2, y2) <= distmin
