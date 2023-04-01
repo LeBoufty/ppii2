6e8736573a6b0b32ebd0f641b258f379fe35d02d
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-04-01 08:20:34 +0200

    Nouveau filtre Utilise la BD telle quelle. Garde les infos qui nous intéressent.

--- BD/filtrestations.py
+++ BD/filtrestations.py
@@ -1,25 +1,55 @@
 from outils import *
 # Ce script python douteux sert à filtrer les stations et les données qu'on veut garder.
 # Il a besoin de la version modifiée de la BD fournie. Elle se trouve sur mon (PYJ) PC (car dans le gitignore)
-data = [i.replace('\n', '').replace('"[', '').replace(']', '').replace(' ', '').replace('"', '').split(',') for i in open('BD/consolidation-etalab-schema-irve-statique-v-2.2.0-20230323.csv', 'r').readlines()]
+nomBD = 'BD/consolidation-etalab-schema-irve-statique-v-2.2.0-20230327.csv'
+
+print('Parsing...')
+data = monstre(open(nomBD, 'r').read())[1:]
+print('Parsing fini.')
 
 # Rafraichissement de la BD
 sortie = open('BD/stations.csv', 'w')
 sortie.write('')
 sortie.close()
 
-sortie = open('BD/stations.csv', 'a')
+def filtre(proprietes:list, distmin:float = 1, concerne:bool = True, libre:bool = True, doublons:bool = False, nomsortie:str = 'stations'):
+    # Màj du nom de la sortie : ne pas oublier de lancer le programme depuis la racine du projet !!
+    nomsortie = 'BD/'+nomsortie+'.csv'
+    print('Début du filtre, sortie sur', nomsortie)
 
-xy_trouves = []
+    # Définition des colonnes à garder
+    a_garder = indices(proprietes)
 
-for ligne in data:
-    if len(ligne) == 4:
-        id, x, y, acces = ligne
-        x = float(x)
-        y = float(y)
-        if (acces == "Accèslibre") and ((x,y) not in xy_trouves) and ("Non" not in id) and not a_proximite(x, y, xy_trouves, 1):
+    # Remise à zéro de la BD
+    sortie = open(nomsortie, 'w')
+    sortie.write(','.join(proprietes) + '\n')
+    sortie.close()
+    sortie = open(nomsortie, 'a')
+
+    # Compteur : à titre indicatif.
+    n = len(data)
+    i = 0
+
+    # Liste des coordonnées trouvées
+    xy_trouves = []
+
+    for ligne in data:
+        # Mise à jour du compteur
+        i += 1
+        if i%5000 == 0:
+            print('Ligne', i, '/', n)
+        # On récupère des données importantes au traitement qui ne sont peut-être pas gardées dans la BD finale
+        x, y = float(ligne[indice('consolidated_longitude')]), float(ligne[indice('consolidated_latitude')])
+        id = ligne[indice('id_station_itinerance')]
+        acces = ligne[indice('condition_acces')]
+        # On ne garde que les propriétés demandées
+        ligne = [ligne[i] for i in a_garder]
+        # On ne garde que les stations qui respectent les conditions données
+        if (acces == "Accès libre" and libre) and ((x,y) not in xy_trouves and not doublons) and ("Non" not in id and concerne) and not a_proximite(x,y,xy_trouves,distmin):
             xy_trouves.append((x,y))
-            sortie.write((id + ',' + str(x) + ',' + str(y) + '\n').replace(' ', ''))
+            sortie.write(','.join(ligne) + '\n')
+    # Et si tout va bien ça n'a toujours pas planté.
+    print('Fini !')
+    sortie.close()
 
-print('Fini !')
-sortie.close()
+filtre(['id_station_itinerance', 'consolidated_longitude', 'consolidated_latitude', 'nbre_pdc'])
--- BD/outils.py
+++ BD/outils.py
@@ -1,6 +1,7 @@
 from math import radians, sin, cos, acos
 
-def distance(x1, y1, x2, y2):
+def distance(x1:float, y1:float, x2:float, y2:float) -> float:
+    """Calcule la distance entre x1,y1 et x2,y2."""
     if x1 == x2 and y1 == y2:
         return 0 # Pour éviter les potentielles erreurs de calcul faisant que la distance soit non-nulle.
     x1 = radians(x1)
@@ -15,7 +16,8 @@ def distance(x1, y1, x2, y2):
     
     return 6371 * acos(A)
 
-def a_proximite(x, y, xy_trouves, distmin = 5):
+def a_proximite(x:float, y:float, xy_trouves:list, distmin = 5) -> bool:
+    """Donne si une station x,y est à moins de distmin d'une autre."""
     i = 0
     if len(xy_trouves) == 0: return False
     x2, y2 = xy_trouves[0]
@@ -23,3 +25,90 @@ def a_proximite(x, y, xy_trouves, distmin = 5):
         x2, y2 = xy_trouves[i]
         i += 1
     return distance(x, y, x2, y2) <= distmin
+
+colonnes = [
+    # La description des propriétés se trouve ici : https://schema.data.gouv.fr/etalab/schema-irve-statique/2.2.0/documentation.html
+    'nom_amenageur',
+    'siren_amenageur',
+    'contact_amenageur',
+    'nom_operateur',
+    'contact_operateur',
+    'telephone_operateur',
+    'nom_enseigne',
+    'id_station_itinerance',
+    'id_station_local',
+    'nom_station',
+    'implantation_station',
+    'adresse_station',
+    'code_insee_commune',
+    'coordonneesXY',
+    'nbre_pdc',
+    'id_pdc_itinerance',
+    'id_pdc_local',
+    'puissance_nominale',
+    'prise_type_ef',
+    'prise_type_2',
+    'prise_type_combo_ccs',
+    'prise_type_chademo',
+    'prise_type_autre',
+    'gratuit',
+    'paiement_acte',
+    'paiement_cb',
+    'paiement_autre',
+    'tarification',
+    'condition_acces',
+    'reservation',
+    'horaires',
+    'accessibilite_pmr',
+    'restriction_gabarit',
+    'station_deux_roues',
+    'raccordement',
+    'num_pdl',
+    'date_mise_en_service',
+    'observations',
+    'date_maj',
+    'cable_t2_attache',
+    'last_modified',
+    'datagouv_dataset_id',
+    'datagouv_resource_id',
+    'datagouv_organization_or_owner',
+    'consolidated_longitude',
+    'consolidated_latitude',
+    'consolidated_code_postal',
+    'consolidated_commune',
+    'consolidated_is_lon_lat_correct',
+    'consolidated_is_code_insee_verified'
+]
+
+def indice(propriete:str) -> int:
+    """Donne l'indice correspondant à la propriété"""
+    i = 0
+    while i < len(colonnes) and colonnes[i] != propriete: i += 1
+    if i == len(colonnes): return None
+    else: return i
+
+def indices(proprietes:list) -> list:
+    """Renvoie la liste des indices correspondant aux propriétés fournies."""
+    return [indice(i) for i in proprietes]
+
+def monstre(fichier:str) -> list:
+    """Sert au parsing des données. Cette fonction est un monstre."""
+    sortie = []
+    # Le .csv étant trop imprévisible, je suis contraint de procéder caractère par caractère.
+    ligne = []
+    quote = False # Sert à définir si on se trouve dans un string. Dans ce cas les , ne séparent pas des informations.
+    param = ''
+    col = 0
+    for i in fichier:
+        if i == '"': quote = not quote
+        if i == ',' and not quote:
+            ligne.append(param)
+            param = ''
+        elif i == '\n' and not quote:
+            ligne.append(param)
+            sortie.append(ligne)
+            ligne = []
+        elif i == '\n' and quote:
+            param += ' '
+        else: param += i
+    return sortie
