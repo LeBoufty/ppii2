3fe55861d594e41140d262b549c209e67007e65d
Titouan Langlais <titouan.langlais@telecomnancy.eu>
2023-05-09 22:38:44 +0200

    Commentaire des lignes :pinched_fingers:

--- src/traitementP2.c
+++ src/traitementP2.c
@@ -3,16 +3,16 @@
 
 //Fonction qui passe le réseau à l'instant n+1
 void traitement(utilisateurtrajet* trajet){
-    utilisateurtrajet* currenttrajet=trajet;
-    while (currenttrajet->next!=NULL){
+    utilisateurtrajet* currenttrajet=trajet;//Liste des utilisateurs
+    while (currenttrajet->next!=NULL){//Parcours tous les utilisateurs
         if (currenttrajet->info->Nb_ticks_attente<=0){//Si l'utilisateur est à une borne
             if (currenttrajet->info->ID_courrant==0){//Si l'utilisateur est à la fin du trajet
                 //destroy trajets
-                add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), 1);
+                add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), 1);//EUHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
             }
             else{//Si sa station n'est pas la dernière
                 if (currenttrajet->info->Nb_ticks_attente<0){//Si la voiture n'est pas chargée                                                                                                                      
-                    currenttrajet->info->Nb_ticks_attente+=1;
+                    currenttrajet->info->Nb_ticks_attente+=1;//Recharge la voiture 
                 }
                 else if (currenttrajet->info->Nb_ticks_attente==0){//Si la voiture est chargée
                 add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), 1);//Libère la place dans la station
@@ -22,13 +22,13 @@ void traitement(utilisateurtrajet* trajet){
             }
         }
         else if (currenttrajet->info->Nb_ticks_attente-1<=0){//Si il arrive à une station
-                if (get_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant))>0){// 
+                if (get_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant))>0){//Regarde si il y a de la place dans la station
                     currenttrajet->info->Nb_ticks_attente=-get_chemin_tab_struct_temps_recharge(currenttrajet->info->chemin,currenttrajet->info->ID_courrant)/ticksparh+1;//Initialise le rechargement
-                    add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), -1)
+                    add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), -1)//Réserve sa place dans la station
                 }
         }
-        else currenttrajet->info->Nb_ticks_attente-=1;
-    currenttrajet=currenttrajet->next;
+        else currenttrajet->info->Nb_ticks_attente-=1;//Si à une distance>1 d'une station
+    currenttrajet=currenttrajet->next;//Prochain utilisateur
     }
 }
 /*
