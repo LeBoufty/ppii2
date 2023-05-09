171cd65fe54d2d0232d0c8204a7cf373aa3ffbea
Titouan Langlais <titouan.langlais@telecomnancy.eu>
2023-05-09 22:16:32 +0200

    V1 de traitement

--- src/traitementP2.c
+++ src/traitementP2.c
@@ -5,51 +5,34 @@
 int* traitement(utilisateurtrajet* trajet){
     utilisateurtrajet* currenttrajet=trajet;
     while (currenttrajet->next!=NULL){
-        if (currenttrajet->info->distance_pro==0){//Si l'utilisateur est à une borne
-            if (){//Si l'utilisateur est à la fin du trajet
+        if (currenttrajet->info->Nb_ticks_attente<=0){//Si l'utilisateur est à une borne
+            if (currenttrajet->info->ID_courrant==0){//Si l'utilisateur est à la fin du trajet
+                //destroy trajets
             }
-            if(){//Si il y a de la place dans la station
-                //Mettre à jour la station    
-                if (){//Si la voiture n'est pas chargée                                                                                                                      
-                    if (){//Si la voiture est chargée pendant le tick
-                        
-                    }
-                    else{//Si la voiture n'est pas chargée pendant le tick
-                    }
+            else{//Si sa station n'est pas la dernière
+                if (currenttrajet->info->Nb_ticks_attente<0){//Si la voiture n'est pas chargée                                                                                                                      
+                    currenttrajet->info->Nb_ticks_attente+=1;
                 }
-                else if (){//Si la voiture est chargée
-                //Mettre à jour la station
-                //enlever le point actuel et passer au prochain
-                //Changer la distance_pro (-1 car c'est un tick)
+                else if (currenttrajet->info->Nb_ticks_attente==0){//Si la voiture est chargée
+                add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), 1);//Libère la place dans la station
+                currenttrajet->info->ID_courrant-=1;//Change de destination
+                currenttrajet->info->Nb_ticks_attente=get_chemin_tab_struct_distance_prochain(currenttrajet->info->chemin,currenttrajet->info->ID_courrant)-1;//Se déplace vers sa destination
                 }
             }
-            else{//Mettre à jour la liste d'attente
-
-            }
         }
-        else {//Si il n'est pas à une borne 
-            currenttrajet->info->distance_pro=currenttrajet->info->distance_pro-1;
+        else if (currenttrajet->info->Nb_ticks_attente-1<=0){
+                if (get_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant))>0){
+                    currenttrajet->info->Nb_ticks_attente=get_chemin_tab_struct_temps_recharge(currenttrajet->info->chemin,currenttrajet->info->ID_courrant);
+                    add_station_tab_nbre_pdc_dispo(currenttrajet->info->chemin->s_tab,get_station_tab_id(currenttrajet->info->chemin->s_tab,currenttrajet->info->ID_courrant), -1)
+                }
         }
+        else currenttrajet->info->Nb_ticks_attente-=1;
     }
 }
-
 /*
   -traitement(info: utilisateurinfo*)->Etat_reseau int* (Grosse fonction)
 	  Utilité : Parcours la liste chainée pour trouver les utilisateur à une distance_de_borne=0, gère les différents cas (Lance le rechargement, file d'attente ou fin de trajet.), gère le rechargement et gère le prochain tick. En gros, elle gère tout le tick de 10min en faisant 1 seul parcours de la liste.
 	  I/O 	Entrée : list_truc la liste des trajets avec leur distance_de_borne
 		      Sortie : retourne l'état du réseau à l'instant n 
 
-
-Mettre à jour la doc !
-
-Chemin ?
-
-Peut-être faire un taux de charge comme : km_restant; km_total; vitesse_de_rechragement(en km/tick)
-
-Liste d'attente ? Liste des files d'attentes /stations ? Liste de tableau des stations ? 
-
-Prochaine étape ? cf chemin à emprunter
-
-Faire une struct stations, parcours%
- 
 */
