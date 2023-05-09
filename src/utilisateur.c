9f81c237849e8bebe33c7da8dd8c7bd8b7caa78f
KRAFFT Arnaud <arnaud.krafft@telecomnancy.eu>
2023-05-09 15:21:28 +0200

    plus que l'erreur du Astar TT UwU

--- src/utilisateur.c
+++ src/utilisateur.c
@@ -16,10 +16,9 @@ utilisateur* create_utilisateur()
 utilisateurinfo* create_utilisateurinfo()
 {
     utilisateurinfo* list_ui=malloc(sizeof(utilisateurinfo));
-    list_ui->Nb_ticks_attente=calloc(1,sizeof(int));
-    list_ui->ID_courrant=calloc(1,sizeof(int));
+    list_ui->Nb_ticks_attente=0;
+    list_ui->ID_courrant=0;
     list_ui->chemin=calloc(1,sizeof(chemin_tab_struct));
-    list_ui->next=NULL;
     return(list_ui);
 }
 
@@ -33,27 +32,27 @@ utilisateurtrajet* create_utilisateurtrajet()
 
 // Crée une liste d'utilisateur remplie aléatoirement
 utilisateur* rdm_utilisateur(voiture_tab* list_v, station_tab* list_s,int n) {
-    utilisateur* list_u=utilisateur_create();
+    utilisateur* list_u=create_utilisateur();
     float lodep, loarr, ladep, laarr;
     int taille_v=list_v->taille;
     int taille_s=list_s->taille;
-    int rdm_v;
-    int rdm_s_dep;
-    int rdm_s_arr;
-    int i;
+    int rdm_v=0;
+    int rdm_s_dep=0;
+    int rdm_s_arr=0;
+    int i=0;
     char* voiture;
     for (i=0;i<n;i++)
     {
-        rdm_v==rand()%taille_v;
-        rdm_s_dep==rand()%taille_s;
-        rdm_s_arr==rand()%taille_s;
-        lodep=get_station_tab_longitude(list_s,rdm_s_dep);
-        loarr=get_station_tab_longitude(list_s,rdm_s_arr);
-        ladep=get_station_tab_latitude(list_s,rdm_s_dep);
-        laarr=get_station_tab_latitude(list_s,rdm_s_arr);
+        rdm_v=rand()%taille_v;
+        rdm_s_dep=rand()%taille_s;
+        rdm_s_arr=rand()%taille_s;
+        lodep=get_station_tab_coord_x(list_s,rdm_s_dep);
+        loarr=get_station_tab_coord_x(list_s,rdm_s_arr);
+        ladep=get_station_tab_coord_y(list_s,rdm_s_dep);
+        laarr=get_station_tab_coord_y(list_s,rdm_s_arr);
         voiture=get_voiture_tab_name(list_v,rdm_v);
 
-        utilisateur* u_suiv = utilisateur_create();
+        utilisateur* u_suiv = create_utilisateur();
         u_suiv->depart->x=list_u->depart->x;
         u_suiv->depart->y=list_u->depart->y;
         u_suiv->arrivee->x=list_u->arrivee->x;
@@ -78,16 +77,12 @@ void utilisateur_trajet_append(utilisateurtrajet* trajet, utilisateurinfo* info)
     
 }
 
-void utilisateur_info_append(utilisateurinfo* info, chemin_tab_struct* chemin, int ID, int tic)
+void utilisateur_info_change(utilisateurinfo* info, chemin_tab_struct* chemin, int ID, int tic)
 {
-    utilisateurinfo* info_suiv=create_utilisateurinfo();
-    info_suiv->chemin=info->chemin;
-    info_suiv->ID_courrant=info->ID_courrant;
-    info_suiv->Nb_ticks_attente=info->Nb_ticks_attente;
     info->chemin=chemin;
     info->ID_courrant=ID;
     info->Nb_ticks_attente=tic;
-    info->next=info_suiv;
+
 }
 
 utilisateurtrajet* trajets(utilisateur* list_u)
@@ -101,9 +96,10 @@ utilisateurtrajet* trajets(utilisateur* list_u)
         utilisateurinfo* info=create_utilisateurinfo();
         corresp_station_tab* corresp = select_point_struct(list_u->depart, list_u->arrivee, tab_s, 1);
         matrice_inf* matrice = generate_adj_matrice(corresp, list_u->depart, list_u->arrivee, tab_s);
-        chemin_tab_struct* chemin=a_star(matrice,list_u->depart,list_u->arrivee,50);
+        chemin_tab_struct* chemin=a_star(matrice,list_u->depart,list_u->arrivee);
         size=size_chemin_tab_struct(chemin);
-        utilisateur_info_append(info,chemin[1:],get_chemin_tab_struct_id_station(chemin,0),get_chemin_tab_struct_distance_prochain(chemin,0)*vitesse/tiksparh);
+        
+        utilisateur_info_change(info,chemin,size_chemin_tab_struct(chemin)-1,get_chemin_tab_struct_distance_prochain(chemin,0)*vitesse/tiksparh);
         utilisateur_trajet_append(trajet,info);
         list_u=list_u->next;
     }
