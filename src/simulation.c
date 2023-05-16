aa73905d8029d3e683a0c7ea2344a9a572a27783
Titouan Langlais <titouan.langlais@telecomnancy.eu>
2023-05-16 20:16:48 +0200

    remove warning stupid

--- src/simulation.c
+++ src/simulation.c
@@ -21,16 +21,16 @@ int main(int argc, char** argv){
     utilisateur* list_u=rdm_utilisateur(tab_v,tab_s,N);
     printf("Utilisateurs initialisés\n");
     utilisateurtrajet* trajet=trajets(list_u,tab_s,tab_v);
-    printf("Trajets initialisés\n");
+    printf("Trajets initialisés\n-------\n");
     
     int i=0;
     while (trajet!=NULL){ //tant qu'il y a des utilisateurs qui ont un trajet
         printf("i=%d\n",i);
-        printf("trajet->next=%p\n",trajet->next);
         createStationFile(i,tab_s);
         traitement(trajet);     
         i++;
     }
+    createStationFile(i,tab_s);
     return 0;
 }
 //43.16477486861467,-1.1408199587551673,6
