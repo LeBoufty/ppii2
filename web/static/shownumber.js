04ae15f6da5716200bd0cc3612c5f988b884c6a3
Pierre-Yves.Jacquier <pierre-yves.jacquier@telecomnancy.eu>
2023-05-19 21:26:22 +0200

    j'ai foiré mon commit SOMALIAAAAA !!!!

--- web/static/shownumber.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("tick");
-var output = document.getElementById("lenombre");
-output.innerHTML = slider.ariaValueMax;
-
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
